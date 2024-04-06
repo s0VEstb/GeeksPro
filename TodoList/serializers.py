9fc7e359c36a59972f3f4b0c76ab0a93aa7ea86d
s0VEstb <attokurov090506@gmail.com>
2024-04-06 16:56:51 +0600

    2nd commit added validator

--- TodoList/serializers.py
+++ TodoList/serializers.py
@@ -8,3 +8,14 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = ['title', 'description', 'completed']
 
 
+class TaskValiditySerializer(serializers.Serializer):
+    title = serializers.CharField(min_length=1, max_length=50)
+    description = serializers.CharField(min_length=1, max_length=150)
+    completed = serializers.BooleanField(default=False)
+
+    def validate_title(self, title):
+        if Task.objects.filter(title=title).exists():
+            raise serializers.ValidationError("Title already exists")
+        return title
+
+
--- TodoList/views.py
+++ TodoList/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from TodoList.models import Task
 
-from TodoList.serializers import TaskSerializer
+from TodoList.serializers import TaskSerializer, TaskValiditySerializer
 
 # Create your views here.
 class TaskListAPIView(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    def post(self, request, *args, **kwargs):
+        validator = TaskValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={'errors': validator.errors})
+
+        title = validator.validated_data['title']
+        description = validator.validated_data['description']
+        completed = validator.validated_data['completed']
+
+        task = Task.objects.create(title=title, description=description, completed=completed)
+        task.save()
+
+        return Response(status=status.HTTP_201_CREATED, data={'message': 'Task created successfully'})
+
+
 
 class TaskDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     lookup_field = "id"
+
+    def put(self, request, *args, **kwargs):
+        task_detail = self.get_object()
+        validator = TaskValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={'errors': validator.errors})
+
+        task_detail.title = validator.validated_data['title']
+        task_detail.description = validator.validated_data['description']
+        task_detail.completed = validator.validated_data['completed']
+        task_detail.save()
+        return Response(status=status.HTTP_201_CREATED, data={'message': 'Task updated successfully'})
+
 
 
 
